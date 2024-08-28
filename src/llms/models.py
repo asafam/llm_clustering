4aaf377842ad7c80cc835dd4db34bec7f825768f
Asaf AchiMordechai <asaf.ach@gmail.com>
2024-08-28 14:59:53 +0300

    Added logging

--- src/llms/models.py
+++ src/llms/models.py
@@ -1,5 +1,6 @@
 from anthropic import AnthropicBedrock
 import ast
+import logging
 
 
 class LLM:
@@ -17,6 +18,7 @@ class Claude(LLM):
         self.aws_region = aws_region
 
     def create_messages(self, prompt: str, max_tokens: int = 4096, temperature=0):
+        logger = logging.getLogger('default')
         client = AnthropicBedrock(
             aws_region=self.aws_region,
         )
@@ -34,6 +36,7 @@ class Claude(LLM):
         )
 
         data_text = response.content[0].text
+        logger.debug("LLM returned message")
         
         try:
             data = ast.literal_eval(data_text)
